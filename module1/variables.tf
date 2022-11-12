1efc5ae014a3dbc42e4198eb0be0519839249238
Yassine Sahouane <46093922+Sahouaneyassine@users.noreply.github.com>
2022-11-12 18:24:11 +0100

    Update variables.tf

--- module1/variables.tf
+++ module1/variables.tf
@@ -1,6 +1,12 @@
 variable "person" {
-  description = "Details of the person"
   type        = list(string)
-
-
+  description = "Details of the person (Prenom,Nom,Num) ? "
+  validation {
+    condition     = length(var.person) == 3
+    error_message = "The list must contains 3 elements (Prenom , Non , Num)"
+  }
+  validation {
+    error_message = "Value Must  contain only numbers."
+    condition =  can( regex("/^\d+$/",element(var.person, 2)) )
+  }
 }
